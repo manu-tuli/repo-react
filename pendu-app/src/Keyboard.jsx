a9df943914782a613fd3ba14e7a7b75ad2b0e73d
manu-tuli <tulinayoemmanuel@gmail.com>
2022-02-17 14:07:15 +0100

    up keyboard

--- pendu-app/src/Keyboard.jsx
+++ pendu-app/src/Keyboard.jsx
@@ -1,34 +1,21 @@
+import './Keyboard.css';
 
-function Keyboard() {
+function Keyboard() {   
+    
+    function GetKey(e){
+        e.preventDefault();
+        console.log(e);
+        console.log('Le lien a été cliqué.');  // envoyer le contenu
+    }
+
+    const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
+    'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+
     return (
-        <div class="letter-container" style= {{display: "flex"}}>
-            <span class="letter" style={{backgroundColor: "white"}}>A</span>
-            <span class="letter" style={{backgroundColor: "white"}}>B</span>
-            <span class="letter" style={{backgroundColor: "white"}}>C</span>
-            <span class="letter" style={{backgroundColor: "white"}}>D</span>
-            <span class="letter" style={{backgroundColor: "white"}}>E</span>
-            <span class="letter" style={{backgroundColor: "white"}}>F</span>
-            <span class="letter" style={{backgroundColor: "white"}}>G</span>
-            <span class="letter" style={{backgroundColor: "white"}}>H</span>
-            <span class="letter" style={{backgroundColor: "white"}}>I</span>
-            <span class="letter" style={{backgroundColor: "white"}}>J</span>
-            <span class="letter" style={{backgroundColor: "white"}}>K</span>
-            <span class="letter" style={{backgroundColor: "white"}}>L</span>
-            <span class="letter" style={{backgroundColor: "white"}}>M</span>
-            <span class="letter" style={{backgroundColor: "white"}}>N</span>
-            <span class="letter" style={{backgroundColor: "white"}}>O</span>
-            <span class="letter" style={{backgroundColor: "white"}}>P</span>
-            <span class="letter" style={{backgroundColor: "white"}}>Q</span>
-            <span class="letter" style={{backgroundColor: "white"}}>R</span>
-            <span class="letter" style={{backgroundColor: "white"}}>S</span>
-            <span class="letter" style={{backgroundColor: "white"}}>T</span>
-            <span class="letter" style={{backgroundColor: "white"}}>U</span>
-            <span class="letter" style={{backgroundColor: "white"}}>V</span>
-            <span class="letter" style={{backgroundColor: "white"}}>W</span>
-            <span class="letter" style={{backgroundColor: "white"}}>X</span>
-            <span class="letter" style={{backgroundColor: "white"}}>Y</span>
-            <span class="letter" style={{backgroundColor: "white"}}>Z</span>
+        <div className="letter-container" style= {{display: "flex"}}>
+            {alphabet.map((letter, index) => <div className="letter" 
+            key={index} value={letter} onClick={GetKey}>{letter}</div>)}
         </div>
     );
 }
-export default Keyboard;
+export default Keyboard; 
